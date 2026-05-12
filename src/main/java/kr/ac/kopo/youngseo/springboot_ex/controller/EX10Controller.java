e183191be08dc774196c09e29d47b40841e5819c
JeongYoungseo <wjddudtjjys@gmail.com>
2026-05-12 09:46:39 +0900

    file upload

---
+++ src/main/java/kr/ac/kopo/youngseo/springboot_ex/controller/EX10Controller.java
@@ -0,0 +1,8 @@
+package kr.ac.kopo.youngseo.springboot_ex.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EX10Controller {
+
+}
--- src/main/java/kr/ac/kopo/youngseo/springboot_ex/domain/Member.java
+++ src/main/java/kr/ac/kopo/youngseo/springboot_ex/domain/Member.java
@@ -1,13 +1,16 @@
 package kr.ac.kopo.youngseo.springboot_ex.domain;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class Member {
-    private String id;
-    private int passwd;
-    private String city;
-    private String gender;
-    private String[] hobby;
-    private String greetings;
+//    private String id;
+//    private int passwd;
+//    private String city;
+//    private String gender;
+//    private String[] hobby;
+//    private String greetings;
+    private String name;
+    private MultipartFile fileImage;
 }
