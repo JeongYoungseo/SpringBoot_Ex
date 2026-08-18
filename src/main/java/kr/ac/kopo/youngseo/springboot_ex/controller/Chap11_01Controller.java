44a07f212cf25f510c78da84b7201f2fbdeb2187
JeongYoungseo <wjddudtjjys@gmail.com>
2026-08-18 10:46:45 +0900

    @ExceptionHandler 예외처리 방법

--- src/main/java/kr/ac/kopo/youngseo/springboot_ex/controller/Chap11_01Controller.java
+++ src/main/java/kr/ac/kopo/youngseo/springboot_ex/controller/Chap11_01Controller.java
@@ -3,6 +3,8 @@ package kr.ac.kopo.youngseo.springboot_ex.controller;
 import kr.ac.kopo.youngseo.springboot_ex.exception.UserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,5 +21,22 @@ public class Chap11_01Controller {
     @GetMapping("/exam02_1")
     public void requestMethod02() throws Exception{
         throw new Exception(new UserException("UserException Message"));
+    }
+
+    @GetMapping("/exam3")
+    public void requestMethod03(){
+        throw new UserException("UserException @ExceptionHandler Message");
+    }
+
+    @ExceptionHandler(UserException.class)
+    public String handlerException(UserException ex, Model model) {
+        model.addAttribute("data1", ex.getMessage());
+        model.addAttribute("data2", ex.getMessage());
+        return "viewPage11_01";
+    }
+
+    @GetMapping("/exam4")
+    public void requestMethod04(){
+        throw new UserException("UserException @ControllerAdvice");
     }
 }
